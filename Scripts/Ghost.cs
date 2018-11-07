1355cdded0f513b50dcf9eff7112da0c6e97b634
Saarg <milsonneau.jean@free.fr>
2018-11-07 18:50:24 +0000

    added licence comment header

--- Scripts/Ghost.cs
+++ Scripts/Ghost.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿/*
+ * This code is part of Arcade Car Physics for Unity by Saarg (2018)
+ * 
+ * This is distributed under the MIT Licence (see LICENSE.md for details)
+ */
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
--- Scripts/Trails/TrailEmitter.cs
+++ Scripts/Trails/TrailEmitter.cs
@@ -1,4 +1,9 @@
-﻿using UnityEngine;
+﻿/*
+ * This code is part of Arcade Car Physics for Unity by Saarg (2018)
+ * 
+ * This is distributed under the MIT Licence (see LICENSE.md for details)
+ */
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
