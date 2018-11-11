42b9633d45ec37f72db3399473067105d171c185
Saarg <milsonneau.jean@free.fr>
2018-11-11 18:12:59 +0000

    starting work on motorbikes

---
+++ Scripts/MotorBike.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace VehicleBehaviour {
+	public class MotorBike : MonoBehaviour {
+
+		Rigidbody _rb;
+
+		WheelVehicle _vehicle;
+
+		[SerializeField] string turnInput = "Horizontal";
+
+		void Start()
+		{
+			_rb = GetComponent<Rigidbody>();
+			_vehicle = GetComponent<WheelVehicle>();
+		}
+
+		void FixedUpdate ()
+		{
+			Quaternion originalRot = transform.localRotation;
+
+			originalRot.eulerAngles = new Vector3(originalRot.eulerAngles.x, originalRot.eulerAngles.y, -_vehicle.Steering);
+
+			transform.localRotation = originalRot;
+		}
+	}
+}
