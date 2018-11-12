9e2e88f3e2432a114afe812fa6c06111659a8a9f
Saarg <milsonneau.jean@free.fr>
2018-11-12 23:19:03 +0000

    improved motorbike gameplay, still a WIP

--- Scripts/MotorBike.cs
+++ Scripts/MotorBike.cs
@@ -7,24 +7,38 @@ namespace VehicleBehaviour {
 	public class MotorBike : MonoBehaviour {
 
 		Rigidbody _rb;
+		Vector3 centerOfMass;
 
 		WheelVehicle _vehicle;
-
-		[SerializeField] string turnInput = "Horizontal";
 
 		void Start()
 		{
 			_rb = GetComponent<Rigidbody>();
 			_vehicle = GetComponent<WheelVehicle>();
+
+			centerOfMass = _rb.centerOfMass;
 		}
 
+		uint logCount = 0;
+		Quaternion targetAngle;
 		void FixedUpdate ()
 		{
-			Quaternion originalRot = transform.localRotation;
+			float speedFactor = Mathf.Clamp01(_vehicle.Speed / 45.0f);
 
-			originalRot.eulerAngles = new Vector3(originalRot.eulerAngles.x, originalRot.eulerAngles.y, -_vehicle.Steering);
+			Vector3 worldUp = transform.InverseTransformVector(Vector3.up);
+			worldUp.z = 0;
+			worldUp.Normalize();
 
-			transform.localRotation = originalRot;
+			targetAngle = Quaternion.Lerp(targetAngle, Quaternion.AngleAxis(-(_vehicle.Steering * Mathf.Clamp01(1 + _vehicle.Throttle)) * speedFactor, Vector3.forward), Time.fixedDeltaTime * 4);
+			Vector3 targetUp = targetAngle * worldUp;
+
+			float angle = Vector3.SignedAngle(Vector3.up, targetUp, Vector3.forward);
+
+			_rb.AddRelativeTorque(new Vector3(0, 0, angle * _rb.mass));
+			// _rb.AddTorque(new Vector3(0, (3 + 10*speedFactor) * angle * _rb.mass, 0));
+
+			Debug.DrawLine(transform.position, transform.position + transform.TransformVector(worldUp), Color.red);
+			Debug.DrawLine(transform.position, transform.position + transform.TransformVector(targetUp), Color.green);
 		}
 	}
 }
